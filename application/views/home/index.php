4d4d4e0201324b838203339d57cedd18a104aa0a
arryfebryan23 <arryfebryan23@gmail.com>
2024-07-17 21:02:13 +0700

    initial commit

---
+++ application/views/home/index.php
@@ -0,0 +1,12 @@
+
+
+    <div class="container">
+    <div class="row mt-3">
+        <div class="col-md-6">
+
+                    
+    <h1>Welcome to my website!</h1>
+        
+        </div>
+    </div>
+</div>
